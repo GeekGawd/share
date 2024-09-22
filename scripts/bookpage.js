0a5d839be9727b181d39d7b8e10650e237b023ae
Suyash Singh <suyashsingh.stem@gmail.com>
2024-09-22 11:10:05 +0530

    Initial Commit

--- scripts/bookpage.js
+++ scripts/bookpage.js
@@ -2,10 +2,7 @@ function clickBookButton() {
     const bookButton = document.getElementById("synopsis-book-button");
   
     if (bookButton) {
-      console.log("Book button found. Clicking the button.");
       bookButton.click();
-    } else {
-      console.error("Button not found.");
     }
   }
   
